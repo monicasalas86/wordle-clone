3d31f9ceafd8d4df36c31bd70146b24acb710b39
monicasalas86 <monicsalas167@gmail.com>
2022-10-27 20:41:04 -0500

    created tiles where words would be entered

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const titleDisplay = document.querySelector('.title-container')
+const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 
 const keys = [
@@ -31,6 +31,26 @@ const keys = [
   'M',
   '«'
 ]
+
+const guessRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement('div')
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement('div')
+    tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+    rowElement.append(tileElement)
+  })
+  tileDisplay.append(rowElement)
+})
 
 const handleClick = () => {
   console.log('clicked')
