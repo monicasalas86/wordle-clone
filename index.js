8a6cd1505dfc55daa6ee6a7b79d758654269ae41
monicasalas86 <monicsalas167@gmail.com>
2022-11-04 15:22:55 -0500

    api key hidden

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PORT = 8000
 const axios = require("axios")
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
@@ -11,7 +12,7 @@ app.get('/word', (req, res) => {
     params: {count: '5', wordLength: '5'},
     headers: {
       'X-RapidAPI-Key': '192cd32f37mshbbf3e9a4bd56c19p1a949ejsn85e1d214811e',
-      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+      'X-RapidAPI-Host': process.env.RAPID_API_KEY
     }
   }
   
